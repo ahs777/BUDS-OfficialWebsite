1e6a32d21205028d239c3ee66a478c5ad39033a2
Ayesha Nadeem <159837017+AyeshaNadeemgithub@users.noreply.github.com>
2024-11-24 14:47:05 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,25 +4,26 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        // Dark Blue Theme
-        'dark-divider': '#475569',
-        'dark-highlight': '#93C5FD',
-        'dark-accent-secondary': '#60A5FA',
-        'dark-accent-primary': '#3B82F6',
-        'dark-text-secondary': '#94A3B8',
-        'dark-text-primary': '#F1F5F9',
-        'dark-secondary-bg': '#334155',
-        'dark-bg': '#1E293B',
-
-        // Light Blue Theme
-        'light-divider': '#E2E8F0',
-        'light-highlight': '#93C5FD',
-        'light-accent-secondary': '#60A5FA',
-        'light-accent-primary': '#3B82F6',
-        'light-text-secondary': '#64748B',
-        'light-text-primary': '#1E293B',
-        'light-secondary-bg': '#FFFFFF',
-        'light-bg': '#F8FAFC',
+        darkBlue: {
+          divider: '#475569',
+          highlight: '#93C5FD',
+          accentSecondary: '#60A5FA',
+          accentPrimary: '#3B82F6',
+          textSecondary: '#94A3B8',
+          textPrimary: '#F1F5F9',
+          secondaryBg: '#334155',
+          background: '#1E293B',
+        },
+        lightBlue: {
+          divider: '#E2E8F0',
+          highlight: '#93C5FD',
+          accentSecondary: '#60A5FA',
+          accentPrimary: '#3B82F6',
+          textSecondary: '#64748B',
+          textPrimary: '#1E293B',
+          secondaryBg: '#FFFFFF',
+          background: '#F8FAFC',
+        },
       },
     },
   },
