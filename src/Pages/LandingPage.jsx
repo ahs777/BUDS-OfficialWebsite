9085fca28bbca5c3b1f0e4f565d9adbd79ad0ddd
Muhammed Basil Irfan Rizvi <baasilrazriz@gmail.com>
2024-11-24 09:50:13 +0300

    changes made

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux'
 
 function LandingPage() {
 const msg=useSelector(state=>state.landing.msg)
-console.log(msg)
+console.log('basil'+msg)
   return (
     <h1 className="text-3xl font-bold underline">
     Hello world!
